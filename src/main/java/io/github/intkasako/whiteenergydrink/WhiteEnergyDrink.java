b614e05fabf1de61100d022809efecf3cd466692
Henri Salarolli <henribrazuca@gmail.com>
2026-02-12 02:34:43 -0300

    Initial commit

---
+++ src/main/java/io/github/intkasako/whiteenergydrink/WhiteEnergyDrink.java
@@ -0,0 +1,18 @@
+package io.github.intkasako.whiteenergydrink;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WhiteEnergyDrink implements ModInitializer {
+	public static final String MOD_ID = "white_energy_drink";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+	}
+}
---
+++ src/main/java/io/github/intkasako/whiteenergydrink/WhiteEnergyDrinkClient.java
@@ -0,0 +1,10 @@
+package io.github.intkasako.whiteenergydrink;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WhiteEnergyDrinkClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
